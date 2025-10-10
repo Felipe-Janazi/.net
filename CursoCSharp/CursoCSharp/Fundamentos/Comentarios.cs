d4c60a36ec80547e0c5aae47554132f15bb69857
Felipe <felipe.janazi@sptech.school>
2025-10-10 17:25:57 -0300

    Primeiro e segundo programas

---
+++ CursoCSharp/CursoCSharp/Fundamentos/Comentarios.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Fundamentos
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    internal class Comentarios
+    {
+        public static void Executar()
+        {
+            // Documentando código 
+            Console.WriteLine("Código claro é sempre melhor!");
+
+            /*
+             * Comentário de múltiplas linhas
+             * Muito utilizado para documentação
+             * do código
+             */
+            Console.WriteLine("O C# tem o XML Comments");
+        }
+    }
+}
