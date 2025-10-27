60621293bd3214126a91e6ae140aece20a82692a
Felipe <felipe.janazi@sptech.school>
2025-10-27 12:01:24 -0300

    Set

---
+++ CursoCSharp/CursoCSharp/Colecoes/ColecoesSet.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Colecoes
+{
+
+    internal class ColecoesSet
+    {
+        public static void Executar()
+        {
+            var livro = new Produto("Game of Thrones", 49.9);
+
+            var carrinho = new List<Produto>();
+            carrinho.Add(livro);
+
+            var combo = new List<Produto>
+            {
+                new Produto ("Camisa", 29.9),
+                new Produto ("Nova temporada", 99.9),
+                new Produto ("Poster", 10.0)
+            };
+
+            carrinho.AddRange(combo);
+            Console.WriteLine(carrinho.Count);
+            carrinho.RemoveAt(3);
+
+            foreach(var item in carrinho)
+            {
+                Console.Write(carrinho.IndexOf(item ));
+                Console.WriteLine($"- {item.Nome} {item.Preco}");
+            }
+
+            Console.WriteLine(carrinho.Count);
+            carrinho.Add(livro);
+            Console.WriteLine(carrinho.Count);
+
+        }
+    }
+}
